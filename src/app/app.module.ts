e212288bb43d58260c6e227f7272d6cf681d09cb
Ceasar6 <vietanh25696@gmail.com>
2020-09-04 14:38:10 +0700

    crud &component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import {FormsModule} from '@angular/forms';
+import { SearchComponent } from './search/search.component';
+import { MassageComponent } from './massage/massage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    SearchComponent,
+    MassageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
